f9e8e4d8ad411445f93124b7b2f1cd89c03c0b8e
joselmm <95531879+joselmm@users.noreply.github.com>
2025-10-15 16:03:45 -0500

    Set up Express server with basic route

--- app.js
+++ app.js
@@ -9,7 +9,18 @@ import { sendNotificationEmail } from "./modules/email-sender.js";
 import { shortUrl } from "./modules/url-shorter.js";
 import fs from "fs";
 import unzipper from "unzipper";
+import express from "express";
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("ok");
+});
+
+app.listen(PORT, () => {
+  console.log(`HTTP server listo en http://localhost:${PORT}`);
+});
 
 
 globalThis.NodeHtmlParser = NodeHtmlParser; // 🔑 Inyectas la dependencia
